f5fade1696c72474eb2f3fe14c79153c2da964dc
Alexander Beliaev <abeliaev@aligntech.com>
2022-07-28 11:32:39 +0300

    checking reflexivity and transitivity

--- book.lean
+++ book.lean
@@ -1,3 +1,13 @@
 def n := 2 
 #eval n^100 -- 1267650600228229401496703205376
+#eval String.append "Hello " "World"
+#check 3
+#check @rfl
+#check @trans
+
+def double (s: String) := String.append s s
+#eval double "hello"
+
+
+
 
