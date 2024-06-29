46342c01f87ea6497c6ee1ae978c16e9abb85665
Alexander Beliaev <beliaevs@gmail.com>
2024-06-29 20:23:26 +0400

    input-output, do notation

---
+++ helloname.lean
@@ -0,0 +1,12 @@
+-- run with
+-- lean --run helloname.lean
+
+def main : IO Unit := do
+  let stdin ← IO.getStdin
+  let stdout ← IO.getStdout
+
+  stdout.putStrLn "How would you like to be addressed?"
+  let input ← stdin.getLine
+  let name := input.dropRightWhile Char.isWhitespace
+
+  stdout.putStrLn s!"Hello, {name}!"
