0ef7e26e6a4ec344dca606221c5bee4a33b8d460
Alexander Beliaev <beliaevs@gmail.com>
2024-06-29 20:16:46 +0400

    structures

--- scratch.lean
+++ scratch.lean
@@ -14,3 +14,28 @@ deriving Repr
 def distance (a: Point)(b: Point): Float := Float.sqrt ((a.x-b.x)^2+(a.y-b.y)^2)
 
 #eval distance {x:=0., y:=0.} {x:=1.0, y:=1.0}
+
+inductive Mybool where
+  | mytrue: Mybool
+  | myfalse: Mybool
+
+#check Mybool.mytrue
+
+structure PPoint (α : Type) where
+  x : α
+  y : α
+deriving Repr
+
+def replaceX (α : Type) (point : PPoint α) (newX : α) : PPoint α :=
+  { point with x := newX }
+
+#check replaceX
+
+#eval [1,2,3]
+#eval List.cons 1 (List.cons 2 List.nil)
+
+#eval List.head? ([] : List Int)
+
+def fives : String × Int := ("five", 5)
+
+#eval fives
