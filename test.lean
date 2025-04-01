09c86d1aa825c3cee295b3e3c4b3a4bd75439e74
Alexander Beliaev <beliaevs@gmail.com>
2025-04-01 23:14:31 +0300

    another sample of recursion over Nat

--- test.lean
+++ test.lean
@@ -1,14 +1,12 @@
 def sample x := x*x+3
 
-def fac(n: Nat) := if n > 0 then n*(fac (n-1)) else 1
+def fac(n: Nat) : Nat :=
+if n > 0 then n*(fac (n-1)) else 1
 
 #eval fac 50
 
+def factorial : Nat → Nat
+ | .zero => 1
+ | .succ m => (.succ m) * (factorial m)
 
-
-
-
-
-
-
-
+#eval factorial 100
